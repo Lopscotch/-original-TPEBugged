342553cfc73311c77eff4de34f5e1ab1ea528e86
Esokami <mollica.es@gmail.com>
2019-07-31 03:23:38 -0400

    Re-Arranged Script folder
    - Added basic UI functions.
    - Added basic enemy damage to player health.

---
+++ PrevailureEffect/Assets/Scripts/Enemies/EnemyBehaviour.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+	public float timeBetweenAttacks = 0.5f;
+	public int attackDamage = 10;
+
+	GameObject player;
+	PlayerHealth playerHealth;
+	EnemyHealth enemyHealth;
+
+	bool playerInRange;
+	float timer;
+
+	void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+		playerHealth = player.GetComponent<PlayerHealth>();
+		enemyHealth = GetComponent<EnemyHealth>();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			playerInRange = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			playerInRange = false;
+		}
+	}
+
+	void Update()
+	{
+		timer += Time.deltaTime;
+
+		if(timer >= timeBetweenAttacks && playerInRange && enemyHealth.currentHealth > 0)
+		{
+			Attack();
+		}
+
+		/*if(playerHealth.currentHealth <= 0)
+		{
+
+		}*/
+	}
+
+	void Attack()
+	{
+		timer = 0f;
+
+		if(playerHealth.currentHealth > 0)
+		{
+			playerHealth.TakeDamage(attackDamage);
+		}
+	}
+}
---
+++ PrevailureEffect/Assets/Scripts/Enemies/EnemyHealth.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+	public int startingHealth = 100;
+	public int currentHealth;
+
+	CapsuleCollider capsuleCollider;
+	bool isDead;
+	
+	void Awake()
+	{
+		capsuleCollider = GetComponent<CapsuleCollider>();
+
+		currentHealth = startingHealth;
+	}
+
+	public void TakeDamage(int amount)
+	{
+		if (isDead)
+			return;
+
+		currentHealth -= amount;
+
+		if(currentHealth <= 0)
+		{
+			Death();
+		}
+	}
+
+	void Death()
+	{
+		isDead = true;
+		capsuleCollider.isTrigger = true;
+
+		Destroy(gameObject, 2f);
+	}
+}
