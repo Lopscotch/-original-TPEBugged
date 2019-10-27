67235bc24e68b07f3e8d767643a9e905b4c91922
Lopscotch <52949060+Lopscotch@users.noreply.github.com>
2019-10-27 19:59:27 -0400

    +PlayerPrefab +scene overhauls
    *Added Player Prefab with moveable bodyparts for animation and weapon reloads
    
    * Copied main scene with prefab into skoalboy scene
    
    *added more assets, i.e openable door with animation, hospitial asset pack,

---
+++ PrevailureEffect/Assets/Scripts/Scope.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scope : MonoBehaviour
+{
+    public Animator animator;
+
+    private bool isScope = false;
+
+    void Update ()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            isScope = !isScope;
+            animator.SetBool("Scope", isScope);
+        }
+    }
+
+}
